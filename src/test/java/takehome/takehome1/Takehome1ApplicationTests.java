21a9db6e504f2107115d3dbcb69c4ee3a832e00c
akbar <anugrahakbar522@gmmail.com>
2022-10-31 19:20:08 +0700

    takehome

---
+++ src/test/java/takehome/takehome1/Takehome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package takehome.takehome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
